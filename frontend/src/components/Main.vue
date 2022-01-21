a39a59898d72d9b4620c7c88d3502eca5a75c12b
juhyojeong <juhyojeong58@gmail.com>
2022-01-21 19:44:36 +0900

    Add: 메인화면 수정

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,114 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="login-container">
+    <h2>회원정보</h2>
+    <div class="login-form">
+      <form>
+        <table class="login-table">
+          <colgroup>
+            <col width="40%" />
+            <col width="*" />
+          </colgroup>
+          <tr>
+            <th>아이디</th>
+            <td>
+              <input type="text" v-model="user_id" ref="user_id" disabled />
+            </td>
+          </tr>
+          <tr>
+            <th>닉네임</th>
+            <td>
+              <input
+                type="password"
+                v-model="password"
+                ref="password"
+                disabled
+              />
+            </td>
+          </tr>
+        </table>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Main",
-  props: {
-    msg: String,
+  data() {
+    return {
+      user_id: "유저명",
+      nickname: "닉네임",
+      error_message: "",
+    };
   },
+  mounted() {},
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style>
+.login-container {
+  padding: 20px 25%;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.login-form {
+  margin: 50px 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.login-table th,
+.login-table td {
+  border-bottom: 1px solid #eee;
 }
-a {
-  color: #42b983;
+.login-table td {
+  padding: 10px;
+}
+.login-table td input {
+  width: 80%;
+  min-height: 30px;
+  padding: 0 10px;
+}
+.btn-login {
+  background: #35495e;
+  color: white;
+  padding: 10px;
+}
+.btn-go-signup {
+  color: #43b984;
+  padding: 10px;
+}
+.error_message {
+  color: red;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 20px;
 }
 </style>
